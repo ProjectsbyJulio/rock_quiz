48deaae55325ad60ed318ba5c35746406d7fc181
Julio Canizalez <juliocanizalez@outlook.com>
2021-04-23 22:36:08 -0600

    :art: [UPDATE] code in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:rock_quiz/splash.dart';
-
+import 'package:flutter/services.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(
+            systemNavigationBarColor: Colors.blueGrey[900],
+            statusBarColor: Colors.blueGrey[900]
+        )
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Rock Quiz",
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: SplashScreen(),
+      home: SplashScreen()
     );
   }
 }
